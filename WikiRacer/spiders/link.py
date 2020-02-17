61f838e769a216dc4b6bf09ecef1e1fd18eb9a8f
Nick Silva <silva.d.nicholas@gmail.com>
2020-02-17 16:39:42 -0600

    adds recursive link-to-link crawl calls

--- WikiRacer/spiders/link.py
+++ WikiRacer/spiders/link.py
@@ -1,21 +1,26 @@
 import scrapy
+import scrapy.exceptions.CloseSpider
 
 
 class LinkSpider(scrapy.Spider):
     name = "link"
 
-    start_urls = ["https://en.wikipedia.org/wiki/Romania"]
+    start_urls = ["https://en.wikipedia.org/wiki/Chicago"]
 
     def parse(self, response):
+
+        linkList = list(set(response.xpath('//div[@id="mw-content-text"]//a[contains(@href, "/wiki/")]/@href').getall()))
+
+        yield {
+            "links": linkList
+        }
         
-        for link in response.xpath("//table[@class='infobox']/tbody/tr"):
-            if not link.xpath(".//table[@class='infobox']/tbody/tr/th/a") is None:
-                yield {
-                    'new_link': link.xpath(".//table[@class='infobox']/tbody/tr/th/a/@href").extract_first()
-                }
-            
-        for link in response.xpath(""):
-            yield {
-                'new_link': link.xpath("")
-            }
-        
+        for next_page in linkList:
+            if "/wiki/iowa" == next_page:
+                print("Hurray Hurray!")
+                raise CloseSpider('Success')
+            if next_page is None or next_page.count("/")>2: pass
+            next_page = "https://en.wikipedia.org" + next_page#.encode("utf-16")
+            print("AHHHHHHHHHHHHHHHHHHHhhhHHHhHHHHH: " + next_page)
+            #next_page_link = response.urljoin(next_page)
+            yield scrapy.Request(url=next_page, callback=self.parse)
